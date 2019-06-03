3551c219d109a3f0d497556eca2518fcff88746b
Jack Baron <jackmbaron@gmail.com>
2019-06-03 23:52:01 +0100

    App root

--- client/ts/App.tsx
+++ client/ts/App.tsx
@@ -1,3 +1,18 @@
 import React, { FunctionComponent } from 'react'
+import styled from 'styled-components'
+import { Button } from './Button'
 
-export const App: FunctionComponent = () => <div>yeet mods</div>
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+
+  -webkit-app-region: drag;
+  -webkit-user-select: none;
+`
+
+export const App: FunctionComponent = () => <Container />
+
